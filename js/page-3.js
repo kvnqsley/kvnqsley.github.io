8b535edad1a363db635b4ea2ff2723c6436c932f
Joseph Kingsley <kingsleyj809@gmail.com>
2023-01-20 18:04:01 +0100

    second commit

--- js/page-3.js
+++ js/page-3.js
@@ -1,4 +1,17 @@
+const online = document.querySelector('#online');
+const storage = document.querySelecto('#storage');
+const profile = document.querySelector('#profile');
+const nxtBtn=document.getElementById('nxt-btn')
 const backBtn=document.querySelector('.back-btn');
 backBtn.addEventListener('click',()=>{
     history.back()
 });
+nxtBtn.addEventListener('click',(e)=>{
+ 
+        if ((storage.checked||profile.checked||online.checked)==false) {
+            e.preventDefault();
+            console.log('not checked');
+        }
+  
+    
+})
