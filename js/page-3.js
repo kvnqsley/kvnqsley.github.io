4252313470c46a6783e0d04bdc2772088f2919e5
Joseph Kingsley <kingsleyj809@gmail.com>
2023-01-20 22:00:38 +0100

    third commit

--- js/page-3.js
+++ js/page-3.js
@@ -1,17 +1,26 @@
 const online = document.querySelector('#online');
-const storage = document.querySelecto('#storage');
+const storage = document.querySelector('#storage');
 const profile = document.querySelector('#profile');
-const nxtBtn=document.getElementById('nxt-btn')
+const nxtBtn=document.getElementById('nxt-btn');
+const errorBox=document.getElementById('error-box');
 const backBtn=document.querySelector('.back-btn');
 backBtn.addEventListener('click',()=>{
     history.back()
 });
 nxtBtn.addEventListener('click',(e)=>{
- 
+//  e.preventDefault()
         if ((storage.checked||profile.checked||online.checked)==false) {
             e.preventDefault();
-            console.log('not checked');
+            document.createElement('div');
+            errorBox.textContent=`Please tick a box`
+
         }
+        setInterval(isChecked,2000)
   
     
 })
+function isChecked() {
+    if ((storage.checked||profile.checked||online.checked)==true) {
+        errorBox.classList.add('inactive')
+    }
+}
