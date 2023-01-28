6075e3cec91fdcdc08e2e7972b691d4517cc462a
Joseph Kingsley <kingsleyj809@gmail.com>
2023-01-28 07:34:18 +0100

    added effects to the subscription plans

--- js/page-2.js
+++ js/page-2.js
@@ -1,16 +1,50 @@
 const back=document.querySelector('.back-btn')
 const monthly=document.querySelector('.monthly');
 const yearly=document.querySelector('.yearly');
+const card1=document.querySelector('.card-1');
+const card2=document.querySelector('.card-2');
+const card3=document.querySelector('.card-3');
+const activeCard=document.querySelector('.active-card');
 
+//trigger the next button with  enter key
+window.addEventListener('keypress',(e)=>{
+    if (e.key=='Enter') {
+        document.querySelector('.btn-link').click()
+    }
+})
 back.addEventListener('click',()=>{
     history.back()
     
 
 })
-console.log(history.length);
+
 const toggle =document.querySelector('.toggle span')
 toggle.addEventListener('click',(e)=>{
 e.currentTarget.classList.toggle('active-toggle')
 monthly.classList.toggle('active-color');
 yearly.classList.toggle('active-color')
 })
+
+//card effects
+window.addEventListener('DOMContentLoaded',()=>{
+    localStorage.setItem('package','arcade')
+})
+card1.addEventListener('click',(e)=>{
+    activeCard.style.transform='translateX(0)';
+    e.currentTarget.style.border='none';
+    localStorage.setItem('package','arcade')
+
+    })
+card2.addEventListener('click',(e)=>{
+activeCard.style.transform='translateX(10em)';
+e.currentTarget.style.border='none';
+localStorage.setItem('package','advanced')
+
+})
+card3.addEventListener('click',(e)=>{
+    activeCard.style.transform='translateX(20em)'
+    e.currentTarget.style.border='none'
+    localStorage.setItem('package','pro')
+    
+    })
+   
--- js/page-4.js
+++ js/page-4.js
@@ -1,4 +1,17 @@
+
+// // console.log(pack);
+const pack =document.querySelector('.package');
+pack.textContent=localStorage.getItem('package');
+
+
+//trigger the next button with  enter key
+window.addEventListener('keypress',(e)=>{
+    if (e.key=='Enter') {
+        document.querySelector('.btn-link').click()
+    }
+})
 const backBtn=document.querySelector('.back-btn');
 backBtn.addEventListener('click',()=>{
     history.back()
 });
+
